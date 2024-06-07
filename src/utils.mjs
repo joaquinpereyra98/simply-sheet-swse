62843381c982bf25855a2c7aa413a63dbe9ec317
joaquinpereyra98 <24190917+joaquinpereyra98@users.noreply.github.com>
2024-06-07 00:40:22 -0300

    FEAT: add ability section RECHORE: move header in a new file.

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,5 +1,6 @@
 export async function prepareTemplates(){
     return loadTemplates([
-        "modules/simply-sheet-swse/template/parts/summary-tab.hbs"
+        "modules/simply-sheet-swse/template/parts/summary-tab.hbs",
+        "modules/simply-sheet-swse/template/parts/sheet-header.hbs"
     ])
 }
