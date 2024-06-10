2aa65cf627d0f4410a9f40e100309b9c92144660
joaquinpereyra98 <24190917+joaquinpereyra98@users.noreply.github.com>
2024-06-10 06:05:39 -0300

    FEAT: skill table with accordion on actor sheet

--- src/actor-sheet/actor-sheet.mjs
+++ src/actor-sheet/actor-sheet.mjs
@@ -1,9 +1,9 @@
-import {SWSEActorSheet} from "../../../../systems/swse/module/actor/actor-sheet.mjs";
+import { SWSEActorSheet } from "../../../../systems/swse/module/actor/actor-sheet.mjs";
 export class SimplySWSEActorSheet extends SWSEActorSheet {
   /** @override */
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ["simply-sheet","swse", "sheet", "actor"],
+      classes: ["simply-sheet", "swse", "sheet", "actor"],
       width: 700,
       height: 600,
       tabs: [
@@ -18,30 +18,64 @@ export class SimplySWSEActorSheet extends SWSEActorSheet {
   /** @override */
   get template() {
     const type = this.actor.type;
-    if(type === 'character' || type === 'npc'){
-      return 'modules/simply-sheet-swse/template/actor-sheet.hbs';
+    if (type === "character" || type === "npc") {
+      return "modules/simply-sheet-swse/template/actor-sheet.hbs";
     }
     return super.template;
   }
-   /** @override */
+  /** @override */
   async getData() {
     const context = super.getData();
-    this.prepareAttributes(context)
-    return context
+    this.prepareAttributes(context);
+    this.prepareSkills(context);
+    return context;
   }
-   /** @override */
-  prepareAttributes(context){
+  /**
+   * Prepare the attributes for the actor based on the system attributes.
+   *
+   * @param {Object} context - The context object containing actor information.
+   *
+   */
+  prepareAttributes(context) {
     const system = context.actor.system;
-    context.abilities =  Object.fromEntries(Object.entries(system.attributes).filter(([key, value]) => value.skip === false));;
+    context.abilities = Object.fromEntries(
+      Object.entries(system.attributes).filter(
+        ([key, value]) => value.skip === false
+      )
+    );
     context.attrGen = system.finalAttributeGenerationType;
-    return context
+    return context;
   }
-   /** @override */
-  activateListeners(html){
-    super.activateListeners(html)
+  prepareSkills(context) {
+    const system = context.actor.system;
+    context.skills = Object.fromEntries(
+      Object.entries(system.skills).map(([key, value]) => [
+        key,
+        { ...value, accordionState: this.accordionStates?.[key] },
+      ])
+    );
+  }
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
 
-    html.find('.dark-score-btn').click(ev => {
+    html.find(".dark-score-btn").click((ev) => {
       this.actor.darkSideScore = $(ev.currentTarget).data("value");
-  });
+    });
+    html.find(".accordion-skill-button").on("click", (ev) => {
+      const btn = ev.currentTarget;
+      const skillID = btn.dataset.id;
+
+      const $accordionBody = $(
+        html.find(`.accordion-skill-body[data-id="${skillID}"]`)
+      );
+      const $icon = $($(btn).find("i.fa-solid.fa-angle-down"));
+
+      this.accordionStates = this.accordionStates || {};
+      this.accordionStates[skillID] = !this.accordionStates[skillID];
+      $icon.css("rotate", this.accordionStates[skillID] ? "180deg" : "0deg");
+
+      $accordionBody.slideToggle();
+    });
   }
 }
