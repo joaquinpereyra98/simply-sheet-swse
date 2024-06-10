3ed336bca874637f45291d1ae610e1f886ea41f7
joaquinpereyra98 <24190917+joaquinpereyra98@users.noreply.github.com>
2024-06-10 06:48:20 -0300

    move panels to templates

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,6 +1,17 @@
 export async function prepareTemplates(){
     return loadTemplates([
         "modules/simply-sheet-swse/template/parts/summary-tab.hbs",
-        "modules/simply-sheet-swse/template/parts/sheet-header.hbs"
+        "modules/simply-sheet-swse/template/parts/sheet-header.hbs",
+        "modules/simply-sheet-swse/template/parts/summary-tab-parts/ability-score-panel.hbs",
+        "modules/simply-sheet-swse/template/parts/summary-tab-parts/defence-panel.hbs",
+        "modules/simply-sheet-swse/template/parts/summary-tab-parts/hit-point-panel.hbs",
+        "modules/simply-sheet-swse/template/parts/summary-tab-parts/threshold-panel.hbs",
+        "modules/simply-sheet-swse/template/parts/summary-tab-parts/speed-panel.hbs",
+        "modules/simply-sheet-swse/template/parts/summary-tab-parts/points-panel.hbs",
+        "modules/simply-sheet-swse/template/parts/summary-tab-parts/shields-panel.hbs",
+        "modules/simply-sheet-swse/template/parts/summary-tab-parts/condition-panel.hbs",
+        "modules/simply-sheet-swse/template/parts/summary-tab-parts/gravity-panel.hbs",
+        "modules/simply-sheet-swse/template/parts/summary-tab-parts/skills-panel.hbs",
+        "modules/simply-sheet-swse/template/parts/summary-tab-parts/dark-side-score-panel.hbs"
     ])
 }
