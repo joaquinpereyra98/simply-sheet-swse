a7fe7d5931e709eb1df9a618af6545aab01488da
joaquinpereyra98 <24190917+joaquinpereyra98@users.noreply.github.com>
2024-06-14 03:22:59 -0300

    FEAT: trained checkbox for every skill FEAT: hide header button, show on cover. FIX: add changes create button FIX: checkbox uses force powers

--- src/actor-sheet/actor-sheet.mjs
+++ src/actor-sheet/actor-sheet.mjs
@@ -1,4 +1,5 @@
 import { SWSEActorSheet } from "../../../../systems/swse/module/actor/actor-sheet.mjs";
+import { onToggle } from "../../../../systems/swse/module/common/listeners.mjs";
 import { createAttackDialog } from "../utils.mjs";
 export class SimplySWSEActorSheet extends SWSEActorSheet {
   /** @override */
@@ -76,9 +77,15 @@ export class SimplySWSEActorSheet extends SWSEActorSheet {
   }
   prepareForcePowers(context) {
     const { powers, secrets, techniques, regimens } = this.actor;
+    
+    const powersFinal = foundry.utils.deepClone(powers).map( i => {
+      const useTracker = this.actor.getFlag("simply-sheet-swse", "useTracker") || {};
+      i.useTracker = useTracker[i.id]
+      return i
+    })
 
     context.forcePowers = {
-      forcePower: powers,
+      forcePower: powersFinal,
       forceSecret: secrets,
       forceTechnique: techniques,
       forceRegimen: regimens,
@@ -148,6 +155,9 @@ export class SimplySWSEActorSheet extends SWSEActorSheet {
     html
       .find("[data-action=create-attack]")
       .on("click", this._onCreateAttack.bind(this));
+    html.find(".uses-force-checkbox").on("change", this._onToggleForceUse.bind(this))
+    html.find("[data-action=increase-uses]").on("click", this._onIncreaseForceUses.bind(this))
+    html.find("[data-action=decrease-uses]").on("click", this._onDecreaseForceUses.bind(this))
   }
   _onIncreaseItemQuantity(event) {
     event.preventDefault();
@@ -158,6 +168,7 @@ export class SimplySWSEActorSheet extends SWSEActorSheet {
     } else {
       item.update({ "system.quantity": item.system.quantity + 1 });
     }
+    
   }
   _onEquipToggle(event) {
     event.preventDefault();
@@ -174,4 +185,45 @@ export class SimplySWSEActorSheet extends SWSEActorSheet {
     const itemData = await createAttackDialog();
     return this.actor.createEmbeddedDocuments("Item", [itemData]);
   }
+  _onIncreaseForceUses(event) {
+    event.preventDefault();
+    const id = event.currentTarget.dataset.itemId;
+    const item = this.actor.items.get(id);
+    let useTracker = this.actor.getFlag("simply-sheet-swse", "useTracker") || {};
+
+    if (!useTracker[item.id]) {
+        useTracker[item.id] = [false];
+    } else {
+        useTracker[item.id].push(false);
+    }
+
+    this.actor.setFlag("simply-sheet-swse", "useTracker", useTracker);
+}
+
+  _onDecreaseForceUses(event) {
+    event.preventDefault();
+    const id = event.currentTarget.dataset.itemId;
+    const item = this.actor.items.get(id);
+    const useTracker = this.actor.getFlag("simply-sheet-swse", "useTracker") || {};
+    
+    if (useTracker[item.id]) {
+        useTracker[item.id].pop();
+    }
+    
+    this.actor.setFlag("simply-sheet-swse", "useTracker", useTracker);
+}
+
+_onToggleForceUse(event) {
+  event.preventDefault();
+  const li = event.currentTarget.closest(".item");
+  const item = this.actor.items.get(li.dataset.itemId);
+  const key = event.currentTarget.dataset.key;
+  let useTracker = this.actor.getFlag("simply-sheet-swse", "useTracker");
+
+  if (useTracker && useTracker[item.id]) {
+      useTracker[item.id][key] = !useTracker[item.id][key];
+      this.actor.setFlag("simply-sheet-swse", "useTracker", useTracker);
+  }
+}
+
 }
--- src/actor-sheet/actor.mjs
+++ src/actor-sheet/actor.mjs
@@ -1,10 +1,10 @@
-import { SWSEActor } from '../../../../systems/swse/module/actor/actor.mjs'
+import { SWSEActor } from "../../../../systems/swse/module/actor/actor.mjs";
 export class SimplySWSEActor extends SWSEActor {
-    _prepareCharacterData(system) {
-        super._prepareCharacterData(system);
-        const defences = ["fortitude", "will", "reflex"]
-        defences.forEach(def => {
-            system.defense[def].total += system.levelSummary;
-        })
-    }
+  _prepareCharacterData(system) {
+    super._prepareCharacterData(system);
+    const defences = ["fortitude", "will", "reflex"];
+    defences.forEach((def) => {
+      system.defense[def].total += system.levelSummary;
+    });     
+  }
 }
